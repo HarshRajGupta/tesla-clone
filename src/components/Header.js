a8bb9bd91caec36d084a0dc5caed6f9d0f001f28
HarshRajGupta <HarshRajGupta@hotmail.com>
2023-07-03 09:49:25 +0530

    deploy0

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,10 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React, { useState } from "react";
-import styled from "styled-components";
-import CancelIcon from "@mui/icons-material/Cancel";
-import { selectCars } from "../features/car/carSlice";
-import { useSelector } from "react-redux";
-import "../App.css";
-
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import CancelIcon from '@mui/icons-material/Cancel';
+import { selectCars } from '../features/car/carSlice';
+import { useSelector } from 'react-redux';
+import '../App.css';
 
 function Header() {
 	const [burgerStatus, setBurgerStatus] = useState(false);
@@ -14,14 +13,20 @@ function Header() {
 	return (
 		<Container>
 			<a href="#">
-		<img src="/images/logo.svg" alt="TESLA" />
+				<img
+					src="/images/logo.svg"
+					alt="TESLA"
+				/>
 			</a>
 			<Menu>
 				{cars &&
 					cars.map((car, index) => (
 						<span>
-							<a key={index} href={"#" + car}>
-								{car}
+							<a
+								key={index}
+								href={'#' + car.id}
+							>
+								{car.name}
 							</a>
 						</span>
 					))}
@@ -44,9 +49,15 @@ function Header() {
 				<ListNav>
 					{cars &&
 						cars.map((car, index) => (
-							<li key={index} className="temp">
-								<a href="/" key={index}>
-									{car}
+							<li
+								key={index}
+								className="temp"
+							>
+								<a
+									href={'#' + car.id}
+									key={index}
+								>
+									{car.name}
 								</a>
 							</li>
 						))}
@@ -101,84 +112,84 @@ function Header() {
 export default Header;
 
 const Container = styled.div`
-  top: 0;
-  left: 0;
-  right: 0;
-  min-height: 60px;
-  position: fixed;
-  display: grid;
-  grid-template-columns: auto auto;
-  align-items: center;
-  justify-content: space-between;
-  padding: 0 20px;
-  z-index: 10;
-  svg {
-    z-index: 50;
-  }
-  height: auto;
+	top: 0;
+	left: 0;
+	right: 0;
+	min-height: 60px;
+	position: fixed;
+	display: grid;
+	grid-template-columns: auto auto;
+	align-items: center;
+	justify-content: space-between;
+	padding: 0 20px;
+	z-index: 10;
+	svg {
+		z-index: 50;
+	}
+	height: auto;
 `;
 
 const Menu = styled.div`
-  position: absolute;
-  min-width: max-content;
-  right: 25%;
-  left: 25%;
-  display: grid;
-  grid-template-columns: auto auto auto auto auto auto;
-  grid-gap: 5px;
-  align-items: center;
-  flex: 1;
-  justify-content: center;
-  a {
-    font-size: 14px;
-    font-weight: 600;
-    color: #181b21;
-    text-transform: uppercase;
-    padding: 4px 8px;
-    flex-wrap: nowrap;
-  }
-  span {
-    margin: 0;
-    padding: 0;
-    transition: 1s;
-  }
-  span:hover {
-    background: hsla(0, 0%, 0%, 0.05);
-    border-radius: 12px;
-  }
-  @media (max-width: 1125px) {
-    display: none;
-  }
+	position: absolute;
+	min-width: max-content;
+	right: 25%;
+	left: 25%;
+	display: grid;
+	grid-template-columns: auto auto auto auto auto auto;
+	grid-gap: 5px;
+	align-items: center;
+	flex: 1;
+	justify-content: center;
+	a {
+		font-size: 14px;
+		font-weight: 600;
+		color: #181b21;
+		text-transform: uppercase;
+		padding: 4px 8px;
+		flex-wrap: nowrap;
+	}
+	span {
+		margin: 0;
+		padding: 0;
+		transition: 1s;
+	}
+	span:hover {
+		background: hsla(0, 0%, 0%, 0.05);
+		border-radius: 12px;
+	}
+	@media (max-width: 1125px) {
+		display: none;
+	}
 `;
 
 const RightMenu = styled.div`
-  display: grid;
-  grid-template-columns: auto auto auto;
-  font-size: 14px;
-  align-items: center;
-  a {
-    padding: 4px 8px;
-    font-weight: 500;
-    color: #181b21;
-    text-transform: uppercase;
-    margin-right: 10px;
-    flex-wrap: nowrap;
-  }
-  span {
-    padding: 8px;
-    transition: 1s;
-  }
-  span:hover {
-    background: hsla(0, 0%, 0%, 0.05);
-    border-radius: 12px;
-  }
-  @media (max-width: 768px) {
-    a:nth-child(1),
-    a:nth-child(2) {
-      display: none;
-    }
-    grid-template-columns: auto;
-  }
+	display: grid;
+	grid-template-columns: auto auto auto;
+	font-size: 14px;
+	align-items: center;
+	a {
+		padding: 4px 8px;
+		font-weight: 500;
+		color: #181b21;
+		text-transform: uppercase;
+		margin-right: 10px;
+		flex-wrap: nowrap;
+	}
+	span {
+		padding: 8px;
+		transition: 1s;
+	}
+	span:hover {
+		background: hsla(0, 0%, 0%, 0.05);
+		border-radius: 12px;
+	}
+	@media (max-width: 768px) {
+		a:nth-child(1),
+		a:nth-child(2) {
+			display: none;
+		}
+		grid-template-columns: auto;
+	}
 `;
 
 const BurgerNav = styled.div`
@@ -197,7 +208,7 @@ text-align: center;
 scroll-snap-type: y mandatory;
 overflow: auto !important;
 transform: ${(props) =>
-		props.show ? "translate(0, 0)" : "translate(100%, 0)"};
+	props.show ? 'translate(0, 0)' : 'translate(100%, 0)'};
 transition: 1s;
 li {
     padding: 12px 15px;
@@ -220,17 +231,17 @@ text-align: start;
 `;
 
 const CustomClose = styled(CancelIcon)`
-  cursor: pointer;
+	cursor: pointer;
 `;
 
 const CloseWrap = styled.div`
-  display: flex;
-  justify-content: flex-end;
+	display: flex;
+	justify-content: flex-end;
 `;
 
 const ListNav = styled.div`
-overflow: auto;
-margin-top: 10px;
-height: 85vh;
-padding-bottom: 15px;
+	overflow: auto;
+	margin-top: 10px;
+	height: 85vh;
+	padding-bottom: 15px;
 `;
--- src/features/car/carSlice.js
+++ src/features/car/carSlice.js
@@ -1,20 +1,20 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  cars: [
-    "Model S",
-    "Model 3",
-    "Model X",
-    "Model Y",
-    "Solar Roof",
-    "Solar Panels",
-  ],
+	cars: [
+		{ name: 'Model S', id: 'Model-S' },
+		{ name: 'Model 3', id: 'Model-3' },
+		{ name: 'Model X', id: 'Model-X' },
+		{ name: 'Model Y', id: 'Model-Y' },
+		{ name: 'Solar Roof', id: 'Solar-Roof' },
+		{ name: 'Accessories', id: 'Accessories' },
+	],
 };
 
 const carSlice = createSlice({
-  name: "car",
-  initialState,
-  reducers: {},
+	name: 'car',
+	initialState,
+	reducers: {},
 });
 
 export const selectCars = (state) => state.car.cars;
